3af0948ce9cef1fd1ec72900a42c17c68dba0768
Julia Bergles <jbergles@smith.edu>
2026-09-20 14:40:24 -0400

    parts 1-3

--- hw01.py
+++ hw01.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
+#        Name: Julia Bergles
 #       Peers: (add any collaborators)
 #  References: (anything you checked to solve this)
 # ------------------------------------------------------
@@ -45,6 +45,21 @@ def main():
     # Part 1: Basic Operations
     # =============================================
     # Your code for part 1 under this line and before the print statements
+x = 27
+y = 1
+a = 1.5
+b = 7
+c = -1
+
+result1 = ((3*x)-(9*y))/((b-c)*(2*a))
+
+print("Part 1: x =", x)
+print("Part 1: y =", y)
+print("Part 1: a =", a)
+print("Part 1: b =", b)
+print("Part 1: c =", c)
+print("Part 1: result =",result1)
+
 
     # End of Part 1 ----------------------
 
@@ -53,6 +68,13 @@ def main():
     # =============================================
     # Your code for part 2 under this line and before the print statements
 
+x = 5
+y = -3
+result2 = (x**2)*(y**4)
+print("Part 2: x =", x)
+print("Part 2: y =", y)
+print("Part 2: result =", result2)
+
     # End of Part 2 ----------------------
 
 
@@ -60,6 +82,12 @@ def main():
     # Part 3: Integer divide
     # =============================================
     # Your code for part 3 under this line and before the print statements
+a = 100 #assigning variable as number of dog treats
+b = 13 #number of dogs
+result3 = a//b
+print("Part 3: a =", a)
+print("Part 3: b =", b)
+print("Part 3: result =", result3)
 
     # End of Part 3 ----------------------
 
